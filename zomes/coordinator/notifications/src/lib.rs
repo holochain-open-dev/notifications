51092ba76e2b9e0f32cbe799d3489d74ddf9b451
Leo <leobensman@gmail.com>
2023-06-08 15:16:44 -0400

    debugging

--- zomes/coordinator/notifications/src/lib.rs
+++ zomes/coordinator/notifications/src/lib.rs
@@ -41,43 +41,44 @@ pub enum Signal {
 #[hdk_extern]
 pub fn handle_notification_tip(data: Tip) -> ExternResult<()> {
     emit_signal("tip received")?;
+    Ok(())
 
-    let zome_call_response = call_remote(
-        agent_info().unwrap().agent_latest_pubkey.into(),
-        "notifications",
-        FunctionName(String::from("custom_handle_notification_tip")),
-        None,
-        data,
-    )?;
+    // let zome_call_response = call_remote(
+    //     agent_info().unwrap().agent_latest_pubkey.into(),
+    //     "notifications",
+    //     FunctionName(String::from("custom_handle_notification_tip")),
+    //     None,
+    //     data,
+    // )?;
 
-    match zome_call_response {
-        ZomeCallResponse::Ok(result) => {
-            // let validated: bool = result.decode().map_err(|err| wasm_error!(String::from(err)))?; // Deserialize byte array
-            // if validated {
-            emit_signal("result")?;
-            // }
-            Ok(())
-        }
-        ZomeCallResponse::NetworkError(err) => {
-            Err(
-                wasm_error!(
-                    WasmErrorInner::Guest(format!("There was a network error: {:?}",
-                    err))
-                ),
-            )
-        }
-        ZomeCallResponse::Unauthorized(a,b,c,d,e) => {
-            Err(
-                wasm_error!(
-                    WasmErrorInner::Guest(format!("There was an unauthorized error: {:?}{:?}{:?}{:?}{:?}",
-                    a,b,c,d,e))
-                ),
-            )
-        }
-        _ => {
-            Err(wasm_error!(WasmErrorInner::Guest(format!("There was an error by call: {:?}", zome_call_response))))
-        },
-    }
+    // match zome_call_response {
+    //     ZomeCallResponse::Ok(result) => {
+    //         // let validated: bool = result.decode().map_err(|err| wasm_error!(String::from(err)))?; // Deserialize byte array
+    //         // if validated {
+    //         emit_signal("result")?;
+    //         // }
+    //         Ok(())
+    //     }
+    //     ZomeCallResponse::NetworkError(err) => {
+    //         Err(
+    //             wasm_error!(
+    //                 WasmErrorInner::Guest(format!("There was a network error: {:?}",
+    //                 err))
+    //             ),
+    //         )
+    //     }
+    //     ZomeCallResponse::Unauthorized(a,b,c,d,e) => {
+    //         Err(
+    //             wasm_error!(
+    //                 WasmErrorInner::Guest(format!("There was an unauthorized error: {:?}{:?}{:?}{:?}{:?}",
+    //                 a,b,c,d,e))
+    //             ),
+    //         )
+    //     }
+    //     _ => {
+    //         Err(wasm_error!(WasmErrorInner::Guest(format!("There was an error by call: {:?}", zome_call_response))))
+    //     },
+    // }
 
     // match zome_call_response {
     //     ZomeCallResponse::Ok(result) => { // ExternIO is a wrapper around a byte array
