534ff6a8558aeb49957ad379c39e70743f59e292
Leo <leobensman@gmail.com>
2023-06-12 15:23:45 -0400

    add was_it_sent function

--- zomes/coordinator/notifications/src/sent_notification.rs
+++ zomes/coordinator/notifications/src/sent_notification.rs
@@ -13,6 +13,49 @@ pub fn retrieve_sent_notifications(_:()) -> ExternResult<Vec<Record>> {
 }
 
 #[hdk_extern]
+pub fn was_it_sent(message_id: String) -> ExternResult<bool> {
+  let sent_notifications_entry_type: EntryType = UnitEntryTypes::SentNotification.try_into()?;
+  let filter = ChainQueryFilter::new()
+      .entry_type(sent_notifications_entry_type)
+      .include_entries(true);
+  let all_sent_notifications_records = query(filter)?;
+
+  let output: bool;
+
+  let all_sent_notifications = all_sent_notifications_records
+    .into_iter()
+    .map(|record| {
+        let sent_notification: SentNotification = record
+            .entry
+            .clone()
+            .into_option()
+            .ok_or(
+                wasm_error!(WasmErrorInner::Guest(
+                    String::from("Could not find the SentNotification")
+                )),
+            )?
+            .try_into()?;
+        Ok(sent_notification)
+    })
+    .collect::<ExternResult<Vec<SentNotification>>>()?;
+
+    let filtered_sent_notifications = all_sent_notifications
+    .into_iter()
+    .filter(|sent_notification| {
+        sent_notification.unique_data == message_id
+    })
+    .collect::<Vec<SentNotification>>();
+
+    if filtered_sent_notifications.len() > 0 {
+      output = true;
+    } else {
+      output = false;
+    }
+
+  Ok(output)
+}
+
+#[hdk_extern]
 pub fn create_sent_notification(
     sent_notification: SentNotification,
 ) -> ExternResult<Record> {
